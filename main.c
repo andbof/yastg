f00fc2d71aceb8f7440327bfff7a705e08a6f6fa
Andreas Bofjall <andreas@gazonk.org>
2012-08-18 12:27:36 +0200

    main.c: server info to server struct

--- main.c
+++ main.c
@@ -20,7 +20,6 @@
 #include "test.h"
 #include "server.h"
 #include "sector.h"
-#include "planet.h"
 #include "base.h"
 #include "inventory.h"
 #include "player.h"
@@ -28,20 +27,21 @@
 #include "parseconfig.h"
 #include "civ.h"
 #include "id.h"
-#include "star.h"
 
 #define PORT "2049"
 #define BACKLOG 16
 
 const char* options = "d";
 int detached = 0;
-int running;
 struct cli_tree *cli_root;
 
 extern int sockfd;
 
-static pthread_t srvthread;
-static int srvfd[2];
+struct server {
+	pthread_t thread;
+	int fd[2];
+	int running;
+};
 
 static void parseopts(int argc, char **argv)
 {
@@ -66,6 +66,14 @@ static void chomp(char* s)
 	for (unsigned int len = strlen(s); len > 0 && s[len - 1] == '\n'; s[len - 1] = '\0', len--);
 }
 
+static void write_msg(int fd, struct signal *msg, char *msgdata)
+{
+	if (write(fd, msg, sizeof(*msg)) < 1)
+		bug("%s", "server signalling fd is closed");
+	if (msg->cnt && write(fd, msgdata, msg->cnt) < 1)
+		bug("%s", "server signalling fd is closed");
+}
+
 static int cmd_help(void *ptr, char *param)
 {
 	cli_print_help(stdout, cli_root);
@@ -74,15 +82,13 @@ static int cmd_help(void *ptr, char *param)
 
 static int cmd_wall(void *ptr, char *param)
 {
+	struct server *server = ptr;
 	if (param) {
 		struct signal msg = {
 			.cnt = strlen(param) + 1,
 			.type = MSG_WALL
 		};
-		if (write(srvfd[1], &msg, sizeof(msg)) < 1)
-			bug("%s", "server signalling fd is closed");
-		if (write(srvfd[1], param, msg.cnt) < 1)
-			bug("%s", "server signalling fd is closed");
+		write_msg(server->fd[1], &msg, param);
 	} else {
 		mprintf("usage: wall <message>\n");
 	}
@@ -91,67 +97,69 @@ static int cmd_wall(void *ptr, char *param)
 
 static int cmd_pause(void *ptr, char *param)
 {
-	struct signal s = {
+	struct server *server = ptr;
+	struct signal msg = {
 		.cnt = 0,
 		.type = MSG_PAUSE
 	};
-	write(srvfd[1], &s, sizeof(s));
+	write_msg(server->fd[1], &msg, NULL);
 	return 0;
 }
 
 static int cmd_resume(void *ptr, char *param)
 {
-	struct signal s = {
+	struct server *server = ptr;
+	struct signal msg = {
 		.cnt = 0,
 		.type = MSG_CONT
 	};
-	write(srvfd[1], &s, sizeof(s));
+	write_msg(server->fd[1], &msg, NULL);
 	return 0;
 }
 
 static int cmd_memstat(void *ptr, char *param)
 {
 	struct mallinfo minfo = mallinfo();
-	mprintf("Memory statistics:\n");
-	mprintf("  Memory allocated with sbrk by malloc:           %d bytes\n", minfo.arena);
-	mprintf("  Number of chunks not in use:                    %d\n", minfo.ordblks);
-	mprintf("  Number of chunks allocated with mmap:           %d\n", minfo.hblks);
-	mprintf("  Memory allocated with mmap:                     %d bytes\n", minfo.hblkhd);
-	mprintf("  Memory occupied by chunks handed out by malloc: %d bytes\n", minfo.uordblks);
-	mprintf("  Memory occupied by free chunks:                 %d bytes\n", minfo.fordblks);
-	mprintf("  Size of top-most releasable chunk:              %d bytes\n", minfo.keepcost);
+	mprintf("Memory statistics:\n"
+	        "  Memory allocated with sbrk by malloc:           %d bytes\n"
+	        "  Number of chunks not in use:                    %d\n"
+	        "  Number of chunks allocated with mmap:           %d\n"
+	        "  Memory allocated with mmap:                     %d bytes\n"
+	        "  Memory occupied by chunks handed out by malloc: %d bytes\n"
+	        "  Memory occupied by free chunks:                 %d bytes\n"
+	        "  Size of top-most releasable chunk:              %d bytes\n",
+		minfo.arena, minfo.ordblks, minfo.hblks, minfo.hblkhd,
+		minfo.uordblks, minfo.fordblks, minfo.keepcost);
 	return 0;
 }
 
 static int cmd_stats(void *ptr, char *param)
 {
-	mprintf("Statistics:\n");
-	mprintf("  Size of universe:          %lu sectors\n", ptrlist_len(univ->sectors));
-	mprintf("  Number of users known:     %s\n", "FIXME");
-	mprintf("  Number of users connected: %s\n", "FIXME");
+	mprintf("Statistics:\n"
+	        "  Size of universe:          %lu sectors\n"
+	        "  Number of users known:     %s\n"
+	        "  Number of users connected: %s\n",
+		ptrlist_len(univ->sectors), "FIXME", "FIXME");
 	return 0;
 }
 
 static int cmd_quit(void *ptr, char *param)
 {
+	struct server *server = ptr;
 	mprintf("Bye!\n");
-	running = 0;
+	server->running = 0;
 	return 0;
 }
 
 int main(int argc, char **argv)
 {
-	int i;
 	char *line = malloc(256); /* FIXME */
 	struct configtree *ctree;
 	struct civ *cv;
 	struct sector *s, *t;
-	struct planet *pl;
-	struct star *sol;
 	size_t st, su;
-	struct sarray *gurka;
-	unsigned int tomat;
 	struct civ *civs = civ_create();
+	struct server server;
 
 	/* Open log file */
 	log_init();
@@ -159,7 +167,7 @@ int main(int argc, char **argv)
 	log_printfn("main", "v%s (commit %s), built %s %s", QUOTE(__VER__), QUOTE(__COMMIT__), __DATE__, __TIME__);
 
 	/* Initialize */
-	running = 1;
+	server.running = 1;
 	srand(time(NULL));
 	mtrandom_init();
 	init_id();
@@ -176,13 +184,13 @@ int main(int argc, char **argv)
 	cli_root = cli_tree_create();
 	if (cli_root == NULL)
 		die("%s", "Unable to allocate memory\n");
-	cli_add_cmd(cli_root, "help", cmd_help, NULL, NULL);
-	cli_add_cmd(cli_root, "wall", cmd_wall, NULL, NULL);
-	cli_add_cmd(cli_root, "pause", cmd_pause, NULL, NULL);
-	cli_add_cmd(cli_root, "resume", cmd_resume, NULL, NULL);
-	cli_add_cmd(cli_root, "stats", cmd_stats, NULL, NULL);
-	cli_add_cmd(cli_root, "memstat", cmd_memstat, NULL, NULL);
-	cli_add_cmd(cli_root, "quit", cmd_quit, NULL, NULL);
+	cli_add_cmd(cli_root, "help", cmd_help, &server, NULL);
+	cli_add_cmd(cli_root, "wall", cmd_wall, &server, NULL);
+	cli_add_cmd(cli_root, "pause", cmd_pause, &server, NULL);
+	cli_add_cmd(cli_root, "resume", cmd_resume, &server, NULL);
+	cli_add_cmd(cli_root, "stats", cmd_stats, &server, NULL);
+	cli_add_cmd(cli_root, "memstat", cmd_memstat, &server, NULL);
+	cli_add_cmd(cli_root, "quit", cmd_quit, &server, NULL);
 
 	/* Load config files */
 	printf("Parsing configuration files\n");
@@ -196,15 +204,15 @@ int main(int argc, char **argv)
 	universe_init(civs);
 
 	/* Start server thread */
-	if (pipe(srvfd) != 0)
+	if (pipe(server.fd) != 0)
 		die("%s", "Could not create server pipe");
-	if (pthread_create(&srvthread, NULL, server_main, &srvfd[0]) != 0)
+	if (pthread_create(&server.thread, NULL, server_main, &server.fd[0]) != 0)
 		die("%s", "Could not launch server thread");
 
 	mprintf("Welcome to YASTG v%s (commit %s), built %s %s.\n\n", QUOTE(__VER__), QUOTE(__COMMIT__), __DATE__, __TIME__);
 	mprintf("Universe has %lu sectors in total\n", ptrlist_len(univ->sectors));
 
-	while (running) {
+	while (server.running) {
 		mprintf("console> ");
 		fgets(line, 256, stdin); /* FIXME */
 		chomp(line);
@@ -218,10 +226,10 @@ int main(int argc, char **argv)
 		.cnt = 0,
 		.type = MSG_TERM
 	};
-	if (write(srvfd[1], &signal, sizeof(signal)) < 1)
+	if (write(server.fd[1], &signal, sizeof(signal)) < 1)
 		bug("%s", "server signalling fd seems closed when sending signal");
 	log_printfn("main", "waiting for server to terminate");
-	pthread_join(srvthread, NULL);
+	pthread_join(server.thread, NULL);
 
 	/* Destroy all structures and free all memory */
 
