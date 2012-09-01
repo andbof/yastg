5d3c3e3fa8e31cfcd86c3699943beea1450bd3b8
Andreas Bofjall <andreas@gazonk.org>
2012-09-01 19:27:35 +0200

    htable.c: case insensitive keys and use list.h
    Each element now duplicates the key data in lower case.
    This also means that an update of the key data in the original position
    (for example, a rename of a sector) no longer directly affects the
    htable and will need a separate update.
    
    Also use list.h instead of home-grown list.

--- htable.c
+++ htable.c
@@ -5,11 +5,12 @@
 #include "common.h"
 #include "htable.h"
 #include "log.h"
+#include "list.h"
 
 /*
  * DJBs "times 33" hash
  */
-static unsigned long hash33(char *key)
+static unsigned long hash33(const char *key)
 {
 	unsigned long hash = 5381;
 	assert(key != NULL);
@@ -26,129 +27,117 @@ void* htable_create()
 	MALLOC_DIE(s, sizeof(*s));
 	memset(s, '\0', sizeof(*s));
 	pthread_rwlock_init(&s->lock, NULL);
+	for (unsigned int i = 0; i < HTABLE_SIZE; i++)
+		INIT_LIST_HEAD(&s->table[i].list);
 	return s;
 }
 
 /* htable_add does not lock the rwlock, this is a feature */
-void htable_add(struct htable *t, char *key, void *data)
+void htable_add(struct htable *t, const char * const key, void *data)
 {
 	assert(t != NULL);
 	assert(key != NULL);
 	assert(data != NULL);
 	int i;
 
-	downcase_valid(key);
+	char *dkey = strdup(key);
+	downcase_valid(dkey);
 
-	unsigned long hash = hash33(key) % HTABLE_SIZE;
-	struct st_elem *prev, *cur, *next;
+	unsigned long hash = hash33(dkey) % HTABLE_SIZE;
+	struct st_elem *pos, *list, *new;
 
-	cur = t->table[hash];
-	if (!cur) {
-		MALLOC_DIE(cur, sizeof(*cur));
-		t->table[hash] = cur;
-		cur->prev = NULL;
-		cur->next = NULL;
-	} else {
-		while (cur) {
-			i = strcmp(cur->data, data);
-			if (i == 0) {
-				bug("key %s already exists in htable %p", key, t);
-			} else if (i < 0) {
-				prev = cur;
-				cur = cur->next;
-				if (cur)
-					next = cur->next;
-				else
-					next = NULL;
-			} else if (i > 0) {
-				prev = cur->prev;
-				next = cur;
-				cur = NULL;
-			}
-		}
-		MALLOC_DIE(cur, sizeof(*cur));
-		cur->prev = prev;
-		cur->next = next;
-		if (prev)
-			prev->next = cur;
-		if (next)
-			next->prev = cur;
-	}
-	cur->key = key;
-	cur->data = data;
+	MALLOC_DIE(new, sizeof(*new));
+	new->key = dkey;
+	new->data = data;
 	t->elements++;
+
+	list_for_each_entry(pos, &t->table[hash].list, list) {
+		i = strcmp(pos->key, new->key);
+		if (i == 0)
+			bug("key %s already exists in htable %p", key, t);
+		else if (i > 0) {
+			list_add_tail(&new->list, &pos->list);
+			return;
+		}
+	}
+	list_add_tail(&new->list, &pos->list);
+}
+
+static struct st_elem* _htable_get(struct htable *t, const char * key)
+{
+	int i;
+	unsigned long hash = hash33(key) % HTABLE_SIZE;
+	struct st_elem *list, *pos;
+	void *ptr;
+
+	list_for_each_entry(pos, &t->table[hash].list, list) {
+		i = strcmp(pos->key, key);
+		if (i == 0)
+			return pos;
+		else if (i > 0)
+			return NULL;
+	}
+
+	return NULL;
 }
 
 /* htable_get does not lock the rwlock, this is a feature */
-void* htable_get(struct htable *t, char *key)
+void* htable_get(struct htable *t, const char * const key)
 {
 	assert(t != NULL);
 	if (key == NULL)
 		return NULL;
 
-	int i;
+	char *dkey = alloca(strlen(key));
+	strcpy(dkey, key);
+	downcase_valid(dkey);
 
-	downcase_valid(key);
+	struct st_elem *tmp;
+	tmp = _htable_get(t, dkey);
 
-	unsigned long hash = hash33(key) % HTABLE_SIZE;
-	struct st_elem *elem;
-	void *ptr;
-
-	elem = t->table[hash];
-	while ((elem != NULL) && ((i = strcmp(elem->key, key)) < 0))
-		elem = elem->next;
-
-	if ((elem != NULL) && (i == 0))
-		ptr = (void*)elem->data;
+	if (tmp)
+		return tmp->data;
 	else
-		ptr = NULL;
-
-	return ptr;
+		return NULL;
 }
 
 /* htable_rm does not lock the rwlock, this is a feature */
-void htable_rm(struct htable *t, char *key)
+void htable_rm(struct htable *t, const char * key)
 {
 	assert(t != NULL);
 	assert(key != NULL);
 
-	downcase_valid(key);
+	char *dkey = strdup(key);
+	downcase_valid(dkey);
 
-	unsigned long hash = hash33(key) % HTABLE_SIZE;
 	struct st_elem *prev, *cur;
 
-	cur = htable_get(t, key);
+	cur = _htable_get(t, dkey);
 	if (cur == NULL)
 		bug("string %s does not exist in htable", key);
 
-	prev = cur->prev;
-	if (prev == NULL) {
-		free(t->table[hash]);
-		t->table[hash] = NULL;
-	} else {
-		prev->next = cur->next;
-		if (cur->next)
-			cur->next->prev = prev;
-		free(cur);
-	}
+	list_del(&cur->list);
+
+	free(cur->key);
+	free(cur);
 	t->elements--;
+
+	free(dkey);
 }
 
 void htable_free(struct htable *t)
 {
 	assert(t != NULL);
 	unsigned long l;
-	struct st_elem *cur;
-	struct st_elem *next;
+	struct st_elem *pos, *tmp;
 
 	pthread_rwlock_wrlock(&t->lock);
 
 	for (l = 0; l < HTABLE_SIZE; l++) {
-		cur = t->table[l];
-		while (cur) {
-			next = cur->next;
-			free(cur);
-			cur = next;
+		list_for_each_entry_safe(pos, tmp, &t->table[l].list, list) {
+			list_del(&pos->list);
+			free(pos->key);
+			free(pos);
 		}
 	}
 
--- htable.h
+++ htable.h
@@ -2,25 +2,26 @@
 #define HAS_HTABLE_H
 
 #include <pthread.h>
+#include "list.h"
 
 #define HTABLE_SIZE 65536
-
-struct htable {
-	struct st_elem* table[HTABLE_SIZE];
-	pthread_rwlock_t lock;
-	unsigned long elements;
-};
 
 struct st_elem {
 	char *key;
 	void *data;
-	struct st_elem *prev, *next;
+	struct list_head list;
+};
+
+struct htable {
+	struct st_elem table[HTABLE_SIZE];
+	pthread_rwlock_t lock;
+	unsigned long elements;
 };
 
 void* htable_create();
-void htable_add(struct htable *t, char *key, void *data);
-void* htable_get(struct htable *t, char *key);
-void htable_rm(struct htable *t, char *key);
+void htable_add(struct htable *t, const char * const key, void *data);
+void* htable_get(struct htable *t, const char * const key);
+void htable_rm(struct htable *t, const char * const key);
 void htable_free(struct htable *t);
 
 #endif
