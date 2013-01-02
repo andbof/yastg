andbof/yastg

commit 3e12d13b632ab0e2a8a662fe1f484fd7252acf91

base_type: move base types to configuration
Previously, base types were hardcoded in data.c. Move them to data/bases
and load them upon startup.

base_type.c
@@ -1,0 +1,205 @@
+#include <stdlib.h>
+#include <string.h>
+#include "common.h"
+#include "base_type.h"
+#include "base.h"
+#include "data.h"
+#include "stringtree.h"
+#include "log.h"
+#include "parseconfig.h"
+#include "universe.h"
+
+char base_zone_names[BASE_ZONE_NUM][8] = {
+	"oceanic",
+	"surface",
+	"orbit",
+	"rogue",
+};
+
+static void base_type_init(struct base_type *type)
+{
+	memset(type, 0, sizeof(*type));
+	INIT_LIST_HEAD(&type->list);
+	ptrlist_init(&type->items);
+}
+
+void base_type_free(struct base_type *type)
+{
+	if (type->name)
+		free(type->name);
+	if (type->desc)
+		free(type->desc);
+	ptrlist_free(&type->items);
+}
+
+static int set_description(struct base_type *type, struct config *conf)
+{
+	if (conf->str)
+		type->desc = strdup(conf->str);
+	else
+		type->desc = strdup("");
+
+	return 0;
+}
+
+static int add_zone(struct base_type *type, struct config *conf)
+{
+	struct config *child;
+	int i;
+	struct list_head zone_root = LIST_HEAD_INIT(zone_root);
+	int zones[BASE_ZONE_NUM];
+
+	for (i = 0; i < BASE_ZONE_NUM; i++)
+		zones[i] = i;
+
+	for (i = 0; i < BASE_ZONE_NUM; i++)
+		st_add_string(&zone_root, base_zone_names[i], &zones[i]);
+
+	list_for_each_entry(child, &conf->children, list) {
+		int *j = st_lookup_string(&zone_root, child->key);
+		if (!j)
+			return -1;
+
+		type->zones[*j] = 1;
+	}
+
+	st_destroy(&zone_root, ST_DONT_FREE_DATA);
+
+	return 0;
+}
+
+static void base_type_item_init(struct base_type_item *item)
+{
+	memset(item, 0, sizeof(*item));
+	ptrlist_init(&item->requires);
+}
+
+static int set_item_capacity(struct base_type_item *item, struct config *conf)
+{
+	item->capacity = conf->l;
+	return 0;
+}
+
+static int set_item_produces(struct base_type_item *item, struct config *conf)
+{
+	item->daily_change += conf->l;
+	return 0;
+}
+
+static int set_item_consumes(struct base_type_item *item, struct config *conf)
+{
+	item->daily_change -= conf->l;
+	return 0;
+}
+
+static int set_item_requires(struct base_type_item *item, struct config *conf)
+{
+	struct item *req = st_lookup_string(&univ.item_names, conf->str);
+
+	if (!req) {
+		log_printfn("config", "unknown item '%s'", conf->str);
+		return -1;
+	}
+
+	ptrlist_push(&item->requires, req);
+	return 0;
+}
+
+static void build_item_cmdtree(struct list_head *root)
+{
+	st_add_string(root, "capacity", set_item_capacity);
+	st_add_string(root, "produces", set_item_produces);
+	st_add_string(root, "consumes", set_item_consumes);
+	st_add_string(root, "requires", set_item_requires);
+}
+
+static int add_item(struct base_type *type, struct config *conf)
+{
+	struct item *item;
+
+	item = st_lookup_string(&univ.item_names, conf->key);
+	if (!item)
+		return -1;
+
+	struct base_type_item *type_item = malloc(sizeof(*type_item));
+	if (!type_item)
+		return -1;
+
+	base_type_item_init(type_item);
+	type_item->item = item;
+
+	struct list_head cmd_root;
+	int (*func)(struct item*, struct config*);
+	build_item_cmdtree(&cmd_root);
+
+	struct config *child;
+	list_for_each_entry(child, &conf->children, list) {
+		func = st_lookup_string(&cmd_root, child->key);
+		if (!func) {
+			log_printfn("config", "unknown base type item key: \"%s\"\n", child->key);
+			continue;
+		}
+
+		if (func(type_item->item, child))
+			continue;
+	}
+
+	st_destroy(&cmd_root, ST_DONT_FREE_DATA);
+	return 0;
+}
+
+static void build_command_tree(struct list_head *root)
+{
+	st_add_string(root, "description", set_description);
+	st_add_string(root, "zones", add_zone);
+	st_add_string(root, "item", add_item);
+}
+
+int load_all_bases(struct list_head * const root)
+{
+	struct list_head conf_root = LIST_HEAD_INIT(conf_root);
+	struct list_head cmd_root = LIST_HEAD_INIT(cmd_root);
+	struct config *conf, *child;
+	struct base_type *type;
+	void (*func)(struct base_type*, struct config*);
+
+	build_command_tree(&cmd_root);
+
+	if (parse_config_file("data/bases", &conf_root))
+		return -1;
+
+	list_for_each_entry(conf, &conf_root, list) {
+		type = malloc(sizeof(*type));
+		if (!type)
+			goto err;
+		base_type_init(type);
+
+		type->name = strdup(conf->key);
+		if (!type->name) {
+			free(type);
+			goto err;
+		}
+
+		list_for_each_entry(child, &conf->children, list) {
+			func = st_lookup_string(&cmd_root, child->key);
+			if (!func) {
+				log_printfn("config", "unknown base type key: \"%s\"\n", child->key);
+				goto err;
+			}
+
+			func(type, child);
+		}
+
+		list_add(&type->list, root);
+		st_add_string(&univ.base_type_names, type->name, type);
+	}
+
+	destroy_config(&conf_root);
+	st_destroy(&cmd_root, 0);
+	return 0;
+
+err:
+	destroy_config(root);
+	st_destroy(&cmd_root, 0);
+	return -1;
+}

base_type.h
@@ -1,0 +1,34 @@
+#ifndef _HAS_BASE_TYPE_H
+#define _HAS_BASE_TYPE_H
+
+#include "ptrlist.h"
+
+enum base_zone {
+	OCEAN,
+	SURFACE,
+	ORBIT,
+	ROGUE,
+	BASE_ZONE_NUM
+};
+
+extern char base_zone_names[BASE_ZONE_NUM][8];
+
+struct base_type_item {
+	struct item *item;
+	int capacity;
+	int daily_change;
+	struct ptrlist requires;
+};
+
+struct base_type {
+	char *name;
+	char *desc;
+	struct list_head list;
+	struct ptrlist items;
+	int zones[BASE_ZONE_NUM];
+};
+
+int load_all_bases(struct list_head * const root);
+void base_type_free(struct base_type *type);
+
+#endif

Makefile.am
@@ -28,6 +28,8 @@ check_LTLIBRARIES = test_module.la
 # optflags: -O3 -funroll-loops
 yastg_SOURCES = base.c \
 		base.h \
+		base_type.c \
+		base_type.h \
 		civ.c \
 		civ.h \
 		cli.c \

base.c
@@ -6,8 +6,8 @@
 #include "data.h"
 #include "stringtree.h"
 #include "log.h"
-#include "parseconfig.h"
 #include "planet.h"
+#include "planet_type.h"
 #include "universe.h"
 
 void base_free(struct base *b)
@@ -28,10 +28,10 @@ static void base_init(struct base *base)
 
 static void base_genesis(struct base *base, struct planet *planet)
 {
-	base->planet = planet;
-	base->type = 0; /* FIXME */
+	unsigned int i, j;
 
-	struct base_type *type = &base_types[base->type];
+	base->planet = planet;
+	base->type = ptrlist_random(&planet->type->base_types);
 	base->docks = 1; /* FIXME */
 
 	/* FIXME: limit loop */
@@ -45,7 +45,12 @@ static void base_genesis(struct base *base, struct planet *planet)
 void base_populate_planet(struct planet* planet)
 {
 	struct base *b;
-	int num = 10; /* FIXME */
+	int num;
+
+	if (ptrlist_len(&planet->type->base_types) > 0)
+		num = 10; /* FIXME */
+	else
+		num = 0;
 
 	pthread_rwlock_wrlock(&univ.basenames_lock);
 

base.h
@@ -1,12 +1,14 @@
 #ifndef _HAS_BASE_H
 #define _HAS_BASE_H
 
+#include "data.h"
 #include "ptrlist.h"
 #include "parseconfig.h"
+#include "base_type.h"
 
 struct base {
 	char *name;
-	unsigned int type;
+	struct base_type *type;
 	int docks;
 	struct planet *planet;
 	struct system *system;

data.c
@@ -8,13 +8,6 @@
  * https://en.wikipedia.org/wiki/Class_M_planet
  * http://www.sttff.net/planetaryclass.html */
 
-struct base_type base_types[BASE_TYPE_N] = {
-	{
-		.name    = "Mining colony",
-		.desc    = "Long description",
-		.zones   = {0, 1, 0, 0},
-	},
-};
 /*
  * Other base types:
  * Settlement, Oceanic city, City, Religious reprieve(?), Military installation,

data.h
@@ -1,24 +1,4 @@
 #ifndef _HAS_DATA_H
 #define _HAS_DATA_H
 
-#include "base.h"
-
-#define BASE_TYPE_N 1
-
-enum base_zone {
-	OCEAN,
-	SURFACE,
-	ORBIT,
-	ROGUE,
-	BASE_ZONE_NUM
-};
-
-struct base_type {
-	char *name;
-	char *desc;
-	int zones[BASE_ZONE_NUM];
-};
-
-extern struct base_type base_types[BASE_TYPE_N];
-
 #endif

data/bases
@@ -1,0 +1,9 @@
+"Mining colony" {
+	description "Long description"
+	zones {
+		surface
+	}
+	item "Iron ore" {
+		capacity 2500
+	}
+}

commit 4c5dcf5b9d1444c515e3fdbe3af1a8dc157c8c06

base_type: fix base type item parsing
A bug introduced in 3e12d13 "base_type: move base types to
configuration" prevented items from being associated with base types
because the wrong string was looked up in the item name table. As no
item was ever added, some other implementation bugs were also hidden.

Fix the bugs and complete the implementation so items can be associated
with base types.

base_type.c
@@ -19,14 +19,19 @@ static void base_type_init(struct base_type *type)
 {
 	memset(type, 0, sizeof(*type));
 	INIT_LIST_HEAD(&type->list);
-	ptrlist_init(&type->items);
+	INIT_LIST_HEAD(&type->items);
 }
 
 void base_type_free(struct base_type *type)
 {
 	free(type->name);
 	free(type->desc);
-	ptrlist_free(&type->items);
+
+	struct base_type_item *i, *_i;
+	list_for_each_entry_safe(i, _i, &type->items, list) {
+		list_del(&i->list);
+		free(i);
+	}
 }
 
 static int set_description(struct base_type *type, struct config *conf)
@@ -124,9 +129,16 @@ static int add_item(struct base_type *type, struct config *conf)
 	if (build_item_cmdtree(&cmd_root))
 		return -1;
 
-	item = st_lookup_string(&univ.item_names, conf->key);
-	if (!item)
+	if (!conf->str) {
+		log_printfn("config", "syntax error after \"item\"");
 		goto err;
+	}
+
+	item = st_lookup_string(&univ.item_names, conf->str);
+	if (!item) {
+		log_printfn("config", "unknown item: \"%s\"", conf->str);
+		goto err;
+	}
 
 	struct base_type_item *type_item = malloc(sizeof(*type_item));
 	if (!type_item)
@@ -147,6 +159,8 @@ static int add_item(struct base_type *type, struct config *conf)
 		if (func(type_item->item, child))
 			continue;
 	}
+
+	list_add(&type_item->list, &type->items);
 
 	st_destroy(&cmd_root, ST_DONT_FREE_DATA);
 	return 0;

base_type.h
@@ -18,13 +18,14 @@ struct base_type_item {
 	int capacity;
 	int daily_change;
 	struct ptrlist requires;
+	struct list_head list;
 };
 
 struct base_type {
 	char *name;
 	char *desc;
 	struct list_head list;
-	struct ptrlist items;
+	struct list_head items;
 	int zones[BASE_ZONE_NUM];
 };
 