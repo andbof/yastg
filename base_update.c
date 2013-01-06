andbof/yastg

commit 8fc777cf48a4d18204b8f79998adbf314973435c

base_update: update items in store at bases
Bases can produce and consume items. A daily amount per item per base is
set in the configuration files. This is accomplished by running a
separate thread with a configurable frequency that goes over all items
in all bases and updating the amount as necessary.

base_update.c
@@ -1,0 +1,135 @@
+#include <limits.h>
+#include <pthread.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#include "base.h"
+#include "item.h"
+#include "log.h"
+#include "universe.h"
+
+#define BASE_UPDATE_INTERVAL 10		/* in seconds, no larger than once a day */
+
+#define SECONDS_PER_DAY (24 * 60 * 60)
+#define BASE_UPDATE_FRACTION (SECONDS_PER_DAY / BASE_UPDATE_INTERVAL)
+
+pthread_t thread;
+volatile int terminate;
+
+pthread_condattr_t termination_attr;
+pthread_cond_t termination_cond;
+pthread_mutex_t termination_lock;
+
+static void update_base(struct base *base, uint32_t iteration)
+{
+	struct base_item *item;
+	long change, mod, fraction_iteration;
+
+	pthread_rwlock_wrlock(&base->items_lock);
+
+	list_for_each_entry(item, &base->items, list) {
+		if (!item->daily_change)
+			continue;
+
+		change = item->daily_change / BASE_UPDATE_FRACTION;
+		mod = item->daily_change % BASE_UPDATE_FRACTION;
+		fraction_iteration = BASE_UPDATE_FRACTION / item->daily_change;
+
+		if (mod && item->daily_change && fraction_iteration
+				&& iteration % fraction_iteration == 0) {
+			if (item->daily_change > 0)
+				change++;
+			else
+				change--;
+		}
+
+		if (change < 0 && item->amount < -change)
+			change = -item->amount;
+		else if (change > 0 && item->max - item->amount < change)
+			change = item->max - item->amount;
+
+		item->amount += change;
+	}
+
+	pthread_rwlock_unlock(&base->items_lock);
+}
+
+static void update_all_bases(uint32_t iteration)
+{
+	struct base *base;
+
+	list_for_each_entry(base, &univ.bases, list)
+		update_base(base, iteration);
+}
+
+static void* base_update_worker(void *ptr)
+{
+	struct timespec next, now;
+	uint32_t iteration = 0;
+
+	if (clock_gettime(CLOCK_MONOTONIC, &next))
+		return NULL;
+
+	do {
+		pthread_rwlock_rdlock(&univ.bases_lock);
+		update_all_bases(iteration);
+		pthread_rwlock_unlock(&univ.bases_lock);
+
+		iteration++;
+		if (iteration >= BASE_UPDATE_FRACTION)
+			iteration = 0;
+
+		next.tv_sec += BASE_UPDATE_INTERVAL;
+
+		do {
+			pthread_mutex_lock(&termination_lock);
+			pthread_cond_timedwait(&termination_cond, &termination_lock, &next);
+			pthread_mutex_unlock(&termination_lock);
+
+			if (clock_gettime(CLOCK_MONOTONIC, &now))
+				break;
+
+		} while (!terminate && now.tv_sec < next.tv_sec);
+
+	} while (!terminate);
+
+	return NULL;
+}
+
+int start_updating_bases(void)
+{
+	int r;
+
+	if (pthread_condattr_init(&termination_attr))
+		return -1;
+
+	if (pthread_condattr_setclock(&termination_attr, CLOCK_MONOTONIC))
+		return -1;
+
+	if (pthread_mutex_init(&termination_lock, NULL))
+		return -1;
+
+	if (pthread_cond_init(&termination_cond, &termination_attr))
+		return -1;
+
+	if (pthread_create(&thread, NULL, base_update_worker, NULL))
+		return -1;
+
+	return 0;
+}
+
+void stop_updating_bases(void)
+{
+	terminate = 1;
+	pthread_mutex_lock(&termination_lock);
+	pthread_cond_signal(&termination_cond);
+	pthread_mutex_unlock(&termination_lock);
+
+	pthread_join(thread, NULL);
+
+	pthread_cond_destroy(&termination_cond);
+	pthread_condattr_destroy(&termination_attr);
+	pthread_mutex_destroy(&termination_lock);
+}

Makefile.am
@@ -30,6 +30,8 @@ yastg_SOURCES = base.c \
 		base.h \
 		base_type.c \
 		base_type.h \
+		base_update.c \
+		base_update.h \
 		civ.c \
 		civ.h \
 		cli.c \

base_update.h
@@ -1,0 +1,7 @@
+#ifndef _HAS_BASE_UPDATE_H
+#define _HAS_BASE_UPDATE_H
+
+int start_updating_bases(void);
+void stop_updating_bases(void);
+
+#endif

configure.ac
@@ -27,6 +27,9 @@ AC_CHECK_LIB([m],[floor],
 AC_CHECK_LIB([pthread],[pthread_create],
              [],
              [AC_MSG_ERROR([libpthread not found])])
+AC_CHECK_LIB([rt],[clock_gettime],
+	     [],
+	     [AC_MSG_ERROR([librt not found])])
 
 AC_CHECK_LIB([ev],[ev_now],
 	     [],
@@ -67,8 +70,8 @@ AC_FUNC_ALLOCA
 AC_FUNC_MALLOC
 AC_FUNC_MMAP
 AC_FUNC_REALLOC
-AC_CHECK_FUNCS([floor localtime_r memset munmap pow socket sqrt strchr strdup \
-		strerror strstr strtol],
+AC_CHECK_FUNCS([clock_gettime floor localtime_r memset munmap pow socket sqrt \
+		strchr strdup strerror strstr strtol],
                [],
                [AC_MSG_ERROR([A standard library function was not found])])
 

server.c
@@ -16,6 +16,7 @@
 #include <semaphore.h>
 #include <ev.h>
 
+#include "base_update.h"
 #include "common.h"
 #include "log.h"
 #include "server.h"
@@ -455,6 +456,7 @@ err:
 void* server_main(void* p)
 {
 	ev_io msg_watcher;
+	pthread_t update_thread;
 	loop = EV_DEFAULT;
 	LIST_HEAD(sockets);
 	LIST_HEAD(watchers);
@@ -467,6 +469,9 @@ void* server_main(void* p)
 
 	if (conndata_init(&conn_data))
 		die("%s", "failed initializing connection data structures");
+
+	if (start_updating_bases())
+		die("%s", "failed starting base update thread");
 
 	initialize_server_sockets(&sockets);
 	if (list_empty(&sockets))
@@ -489,6 +494,7 @@ void* server_main(void* p)
 	disconnect_peers(loop);
 	stop_and_free_server_watchers(&watchers, loop);
 	close_and_free_sockets(&sockets);
+	stop_updating_bases();
 
 	/*
 	 * We'd like to free all resources allocated by libev here, but

commit 791ed243992714f176213e559d9a1886b47328d7

base_update: fix races in terminate flag
Move writing and reading the terminate flag within the pthread mutex
calls, remove the unnecessary volatile modifier and add an additional
flag check before calling pthread_cond_timedwait(). This fixes a data
race between the main and the base_update thread introduced in 8fc777c
"base_update: update items in store at bases".

This was noticable by a hang of BASE_UPDATE_INTERVAL seconds after
calling quit immediately after startup if compiled with optimization
turned on.

base_update.c
@@ -17,7 +17,7 @@
 #define BASE_UPDATE_FRACTION (SECONDS_PER_DAY / BASE_UPDATE_INTERVAL)
 
 pthread_t thread;
-volatile int terminate;
+int terminate;
 
 pthread_condattr_t termination_attr;
 pthread_cond_t termination_cond;
@@ -102,6 +102,10 @@ static void* base_update_worker(void *ptr)
 
 		do {
 			pthread_mutex_lock(&termination_lock);
+			if (terminate) {
+				pthread_mutex_unlock(&termination_lock);
+				break;
+			}
 			pthread_cond_timedwait(&termination_cond, &termination_lock, &next);
 			pthread_mutex_unlock(&termination_lock);
 
@@ -139,8 +143,8 @@ int start_updating_bases(void)
 
 void stop_updating_bases(void)
 {
-	terminate = 1;
 	pthread_mutex_lock(&termination_lock);
+	terminate = 1;
 	pthread_cond_signal(&termination_cond);
 	pthread_mutex_unlock(&termination_lock);
 